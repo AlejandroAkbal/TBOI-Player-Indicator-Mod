2177263bdc287b21b1d03f4d1b13d60eb1cc89ff
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-05-23 13:19:50 +0200

    feat: add initial working version

--- player-indicator/src/main.ts
+++ player-indicator/src/main.ts
@@ -1,14 +1,63 @@
+const GAME = Game();
+
 // Register the mod
-// (which will make it show up in the list of mods on the mod screen in the main menu)
-const playerIndicator = RegisterMod("playerIndicator", 1);
+const PLAYER_INDICATOR_MOD = RegisterMod("Player Indicator", 1);
+
+enum Config {
+  DisableWithOnlyOnePlayer = 0,
+
+  Font = "font/upheaval.fnt",
+  FONT_X_OFFSET = -11,
+  FONT_Y_OFFSET = 5,
+  FONT_SCALE = 1,
+}
 
 // Define callback functions
-function postGameStarted() {
-  Isaac.DebugString("Callback triggered: MC_POST_GAME_STARTED");
+function postRender() {
+  checkForPlayers();
+}
+
+function checkForPlayers() {
+  const NUM_PLAYERS = GAME.GetNumPlayers();
+
+  // Exit
+  if (Config.DisableWithOnlyOnePlayer === 1 && NUM_PLAYERS <= 1) {
+    return;
+  }
+
+  for (let index = 0; index < NUM_PLAYERS; index++) {
+    const PLAYER = Isaac.GetPlayer(index);
+
+    if (!PLAYER) {
+      return;
+    }
+
+    const PLAYER_NUMBER_STRING = `P${PLAYER.ControllerIndex}`;
+
+    drawStringBelowPlayer(PLAYER_NUMBER_STRING, PLAYER);
+  }
+}
+
+function drawStringBelowPlayer(string: string, player: EntityPlayer) {
+  const PLAYER_POSITION = Isaac.WorldToScreen(player.Position);
+
+  const GAME_FONT = Font();
+  GAME_FONT.Load(Config.Font);
+
+  GAME_FONT.DrawStringScaledUTF8(
+    string,
+    PLAYER_POSITION.X + Config.FONT_X_OFFSET,
+    PLAYER_POSITION.Y + Config.FONT_Y_OFFSET,
+    Config.FONT_SCALE,
+    Config.FONT_SCALE,
+    KColor(255, 255, 255, 1),
+    0,
+    true,
+  );
 }
 
 // Register callbacks
-playerIndicator.AddCallback(ModCallbacks.MC_POST_GAME_STARTED, postGameStarted);
+PLAYER_INDICATOR_MOD.AddCallback(ModCallbacks.MC_POST_RENDER, postRender);
 
 // Print an initialization message to the "log.txt" file
-Isaac.DebugString("player-indicator initialized.");
+Isaac.DebugString("Player Indicator initialized.");
