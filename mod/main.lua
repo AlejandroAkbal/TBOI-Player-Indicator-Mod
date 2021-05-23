d84889589c316b5a03b12da8ed4a20dc0ac4e258
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2021-05-23 18:42:51 +0200

    v1.0.5

--- mod/main.lua
+++ mod/main.lua
@@ -21,9 +21,35 @@ ____modules = {
 ["constants"] = function() --[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
 local ____exports = {}
 ____exports.VERSION = "v1.0.2"
+____exports.Config = Config or ({})
+____exports.Config.DisableWithOnlyOnePlayer = 0
+____exports.Config[____exports.Config.DisableWithOnlyOnePlayer] = "DisableWithOnlyOnePlayer"
+____exports.Config.Font = "font/upheaval.fnt"
+____exports.Config.FONT_X_OFFSET = -11
+____exports.Config[____exports.Config.FONT_X_OFFSET] = "FONT_X_OFFSET"
+____exports.Config.FONT_Y_OFFSET = 5
+____exports.Config[____exports.Config.FONT_Y_OFFSET] = "FONT_Y_OFFSET"
+____exports.Config.FONT_SCALE = 1
+____exports.Config[____exports.Config.FONT_SCALE] = "FONT_SCALE"
+____exports.PLAYER_COLOR_ARRAY = {
+    KColor(255, 255, 255, 1),
+    KColor(0, 255, 255, 1),
+    KColor(0, 255, 0, 1),
+    KColor(255, 0, 255, 1),
+    KColor(255, 255, 0, 1),
+    KColor(255, 0, 0, 1),
+    KColor(0, 255, 0, 1),
+    KColor(0, 0, 255, 1),
+    KColor(0, 0, 0, 1)
+}
 return ____exports
 end,
 ["main"] = function() --[[ Generated with https://github.com/TypeScriptToLua/TypeScriptToLua ]]
+local ____exports = {}
+local ____constants = require("constants")
+local Config = ____constants.Config
+local PLAYER_COLOR_ARRAY = ____constants.PLAYER_COLOR_ARRAY
+local GAME, shouldRender, checkForPlayers, drawStringBelowPlayer
 function shouldRender(self)
     local CURRENT_ROOM = GAME:GetRoom()
     if GAME:IsPaused() then
@@ -52,34 +78,16 @@ function checkForPlayers(self)
         end
     end
 end
-function drawStringBelowPlayer(self, ____string, player)
+function drawStringBelowPlayer(self, stringToDraw, player)
     local PLAYER_POSITION = Isaac.WorldToScreen(player.Position)
     local GAME_FONT = Font()
     GAME_FONT:Load(Config.Font)
-    GAME_FONT:DrawStringScaledUTF8(
-        ____string,
-        PLAYER_POSITION.X + Config.FONT_X_OFFSET,
-        PLAYER_POSITION.Y + Config.FONT_Y_OFFSET,
-        Config.FONT_SCALE,
-        Config.FONT_SCALE,
-        KColor(255, 255, 255, 1),
-        0,
-        true
-    )
+    local PLAYER_COLOR = PLAYER_COLOR_ARRAY[player.ControllerIndex + 1] or PLAYER_COLOR_ARRAY[1]
+    GAME_FONT:DrawStringScaledUTF8(stringToDraw, PLAYER_POSITION.X + Config.FONT_X_OFFSET, PLAYER_POSITION.Y + Config.FONT_Y_OFFSET, Config.FONT_SCALE, Config.FONT_SCALE, PLAYER_COLOR, 0, true)
 end
 GAME = Game()
-PLAYER_INDICATOR_MOD = RegisterMod("Player Indicator", 1)
-Config = Config or ({})
-Config.DisableWithOnlyOnePlayer = 0
-Config[Config.DisableWithOnlyOnePlayer] = "DisableWithOnlyOnePlayer"
-Config.Font = "font/upheaval.fnt"
-Config.FONT_X_OFFSET = -11
-Config[Config.FONT_X_OFFSET] = "FONT_X_OFFSET"
-Config.FONT_Y_OFFSET = 5
-Config[Config.FONT_Y_OFFSET] = "FONT_Y_OFFSET"
-Config.FONT_SCALE = 1
-Config[Config.FONT_SCALE] = "FONT_SCALE"
-function postRender(self)
+local PLAYER_INDICATOR_MOD = RegisterMod("Player Indicator", 1)
+local function postRender(self)
     if not shouldRender(nil) then
         return
     end
@@ -87,6 +95,7 @@ function postRender(self)
 end
 PLAYER_INDICATOR_MOD:AddCallback(ModCallbacks.MC_POST_RENDER, postRender)
 Isaac.DebugString("Player Indicator initialized.")
+return ____exports
 end,
 }
 return require("main")
